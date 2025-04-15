15612b1a4640d5f1ad642bab91c4eaf098b9db4a
Connor <Mortiszy@outlook.com>
2025-04-15 17:32:41 +1000

    minor adjustments

--- MTRX2700 C Timers/Src/main.c
+++ MTRX2700 C Timers/Src/main.c
@@ -18,7 +18,9 @@ int main(void)
 
     // testing.
     timer_init(1000, led_blink_sequence);
-    timer_oneshot(4000, led_flash);
+    timer_oneshot(2000, led_flash);
+    set_timer_period(500);
+    timer_oneshot(2000, led_flash_left);
 
     while (1) {
     	// idle loop.
